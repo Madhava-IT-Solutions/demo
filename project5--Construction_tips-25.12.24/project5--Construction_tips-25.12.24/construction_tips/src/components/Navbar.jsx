aa8924b32032ea02d711c4d610f562fc6fc27914
madhavaitsolutions <madhavaitsolutions@gmail.com>
2025-01-18 17:05:28 +0530

    SSV Construction Tips Folder

--- project5--Construction_tips-25.12.24/project5--Construction_tips-25.12.24/construction_tips/src/components/Navbar.jsx
+++ project5--Construction_tips-25.12.24/project5--Construction_tips-25.12.24/construction_tips/src/components/Navbar.jsx
@@ -1,31 +1,16 @@
-
-// components/Navbar.js
 import React from 'react';
 import { Link } from 'react-router-dom';
-
-
 import './Navbar.css';
 
-import economicalImg from '../components/images/economical.jpg';
-import longlifeImg from '../components/images/longlife.jpg';
-import qualityImg from '../components/images/quality.jpg';
-import fasterConstructionImg from '../components/images/FasterConstruction.jpg';
-import materialSelectionImg from '../components/images/MaterialSelection.jpg';
-import safetyImg from '../components/images/safety.jpg';
-import efficientImg from '../components/images/Efficient.jpg';
-import lowMaintenanceImg from '../components/images/lowMaintenance.jpg';
-
-
-
 const categories = [
-  { name: "Economical Construction", image: economicalImg },
-  { name: "Construction methods for Long Life", image: longlifeImg },
-  { name: "Quality Construction", image: qualityImg },
-  { name: "Faster Construction", image: fasterConstructionImg },
-  { name: "Material Selection", image: materialSelectionImg },
-  { name: "Construction with Safety", image: safetyImg },
-  { name: "Efficient Construction", image: efficientImg },
-  { name: "Low Maintenance", image: lowMaintenanceImg }
+  { name: "Economical Construction", image: "Economical.jpg" },
+  { name: "Construction methods for Long Life", image: "longlife.jpg" },
+  { name: "Quality Construction", image: "quality.jpg" },
+  { name: "Faster Construction", image: "FasterConstruction.jpg" },
+  { name: "Material Selection", image: "MaterialSelection.jpg" },
+  { name: "Construction with Safety", image: "safety.jpg" },
+  { name: "Efficient Construction", image: "Efficient.jpg" },
+  { name: "Low Maintenance", image: "lowMaintenance.jpg" }
 ];
 
 const Navbar = () => {
@@ -33,7 +18,11 @@ const Navbar = () => {
     <nav>
       <div className="categories-container">
         {categories.map((category, index) => (
-          <div key={index} className="category-card" style={{ backgroundImage: `url(${category.image})` }}>
+          <div 
+            key={index} 
+            className="category-card" 
+            style={{ backgroundImage: `url(/images/${category.image})` }} // Adjusted path
+          >
             <p></p>
             <Link to={`/category/${category.name.replace(/\s+/g, '-')}`} className="category-link">
               {category.name}
